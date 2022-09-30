4d17a3f57c3064dd785448964cad2503f9040e69
asvens85 <anders.svensson.5@volvocars.com>
2022-09-30 14:02:03 +0200

    Added serial to sketch

--- src/mario-lamp-arduino/mario-lamp-arduino.ino
+++ src/mario-lamp-arduino/mario-lamp-arduino.ino
@@ -1,9 +1,12 @@
 //
 //
+//
+//
 // the setup function runs once when you press reset or power the board
 void setup() {
   // initialize digital pin LED_BUILTIN as an output.
   pinMode(LED_BUILTIN, OUTPUT);
+  Serial.begin(9600); 
 }
 
 // the loop function runs over and over again forever
@@ -12,6 +15,10 @@ void loop() {
   delay(1000);                       // wait for a second
   digitalWrite(LED_BUILTIN, LOW);    // turn the LED off by making the voltage LOW
   delay(1000);                       // wait for a second
+  Serial.println("Hello world!"); 
 }
+//
+//
+//
 //
 //
