c0de4dd4f34ec839fe1143e7b8b234bb9c41bac1
asvens85 <anders.svensson.5@volvocars.com>
2022-10-16 17:38:08 +0200

    BAck to old pinout

--- src/mario-lamp-arduino/led_events.h
+++ src/mario-lamp-arduino/led_events.h
@@ -1,9 +1,9 @@
 #ifndef LED_EVENTS_H
 #define LED_EVENTS_H
 
-#define LOW_LED_PIN 8
-#define MID_LED_PIN 9
-#define HIGH_LED_PIN 13
+#define LOW_LED_PIN 10
+#define MID_LED_PIN 11
+#define HIGH_LED_PIN 12
 
 #include <Arduino.h>
 void increase(int fromState);
