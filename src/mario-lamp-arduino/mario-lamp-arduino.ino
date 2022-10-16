2bb25d491bd394c563d66526086f5e2ceabe3789
asvens85 <anders.svensson.5@volvocars.com>
2022-10-16 17:21:19 +0200

    Even more test code for blink debug

--- src/mario-lamp-arduino/mario-lamp-arduino.ino
+++ src/mario-lamp-arduino/mario-lamp-arduino.ino
@@ -26,12 +26,21 @@
 void setup() {
   // initialize digital pin LED_BUILTIN as an output.
   pinMode(LED_BUILTIN, OUTPUT);
+  pinMode(10, OUTPUT);
+  pinMode(11, OUTPUT);
+  pinMode(12, OUTPUT);
 }
 
 // the loop function runs over and over again forever
 void loop() {
   digitalWrite(LED_BUILTIN, HIGH);   // turn the LED on (HIGH is the voltage level)
+  digitalWrite(10, HIGH);   // turn the LED on (HIGH is the voltage level)
+  digitalWrite(11, HIGH);   // turn the LED on (HIGH is the voltage level)
+  digitalWrite(12, HIGH);   // turn the LED on (HIGH is the voltage level)
   delay(1000);                       // wait for a second
   digitalWrite(LED_BUILTIN, LOW);    // turn the LED off by making the voltage LOW
+  digitalWrite(10, LOW);    // turn the LED off by making the voltage LOW
+  digitalWrite(11, LOW);    // turn the LED off by making the voltage LOW
+  digitalWrite(12, LOW);    // turn the LED off by making the voltage LOW
   delay(1000);                       // wait for a second
 }
